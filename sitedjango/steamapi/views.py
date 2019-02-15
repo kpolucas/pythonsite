2a26fc66c92733eb653727c986e8040801de935c
root <root@chorga.chorga>
2019-02-15 12:47:57 -0300

    api sin html

--- sitedjango/steamapi/views.py
+++ sitedjango/steamapi/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    import requests
+     
+    #Funciona pero no formatea html
+    #payload={'appid': '440', 'count': '3'} # parametros get
+    #r = requests.get('https://api.steampowered.com/ISteamNews/GetNewsForApp/v2', params=payload) 
+    #jr = r.json() # seguro hay una manera menos tumba
+    #print(jr['appnews']['newsitems'][0]['contents'])
+    #c = {'api_response': jr['appnews']['newsitems'][0]['contents']}
+    #return render(request, 'index.html', c)
+    return render(request, 'index.html', {'api_response': '<b>peron</b>'})

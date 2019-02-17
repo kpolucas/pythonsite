a18fe4cb83dd310fb7046605dcab406b40572061
chorga <kpolucas88@gmail.com>
2019-02-17 16:26:20 -0300

    machetes

--- macheteapi.py
+++ macheteapi.py
@@ -6,3 +6,12 @@ r = requests.get('https://api.steampowered.com/ISteamNews/GetNewsForApp/v2', par
 #print(r.content)
 jr = r.json() # seguro hay una manera menos tumba
 print(jr['appnews']['newsitems'][0]['contents'])
+
+
+
+
+#steam api
+https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_.28v0001.29
+http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=C4A47AC9177F2BB6D651A010C5DAD268&steamids=76561198089980282
+https://steamcommunity.com/dev
+https://steamcommunity.com/dev/apikey

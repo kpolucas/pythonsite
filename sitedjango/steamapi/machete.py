e053609106af7ed2b96df1ae420de28086b1ef7d
root <root@chorga.chorga>
2019-02-19 12:25:56 -0300

    housekeeping

--- sitedjango/steamapi/machete.py
+++ sitedjango/steamapi/machete.py
@@ -1,3 +1,4 @@
+'''
 import requests
 import json
 
@@ -5,10 +6,10 @@ def gamesPlayedImgUrl(key,steamid):
     payload={'key': key, 'steamid': steamid, 'format': 'json'}
     r = requests.get('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001', params=payload)
     gamesplayed = json.loads(r.text)
-    count = 0
     urls = []
     for i in gamesplayed['response']['games']:
         urls.append('https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/' + str(i['appid']) + '/' + str(i['img_logo_url']) + '.jpg')
     return urls
 
 print(gamesPlayedImgUrl('',''))
+'''

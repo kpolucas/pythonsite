f68c3d3869c134ea75324bc1ae2f2d0b2d9594ae
chorga <kpolucas88@gmail.com>
2019-03-12 17:46:53 -0300

    nplscrapper

--- 1-nlpscrapper/nplscrapper.py
+++ 1-nlpscrapper/nplscrapper.py
@@ -1,22 +1,22 @@
 from bs4 import BeautifulSoup
-import os
+import os, requests
 
-soup = BeautifulSoup(open("./kotaku.html"), "html.parser")
-#busco links a las notas
+#bajo la pagina y la cargo en beautifulsoup
+page = requests.get("https://kotaku.com")
+soup = BeautifulSoup(page.content, "html.parser")
+#busco links a las notas. fugly, but it works
 curationMountainDiv = soup.find('div', {'class': 'js_curation-mountain curation-mountain Equal'})
+linksParaScrappear = []
 for i in curationMountainDiv.find_all('div', {'class':'curation-module__item js_curation-item box'}):
-    linksParaScrappear = i.get('data-permalink')
+    linksParaScrappear.append(i.get('data-permalink'))
 
-
-
-
-### Falta meter adentro de un FOR con los primeros 3 resultados de arriba
-### Falta cambiar los BeautifulSoup de texto a wget o similar pythonezco
-
-soup = BeautifulSoup(open("./kotaku2.html"), "html.parser")
-#scrapeo las notas
-curationMountainDiv = soup.find('div', {'class', 'post-content entry-content js_entry-content'}).text
-#brujeria que borra emptylines https://stackoverflow.com/questions/1140958/whats-a-quick-one-liner-to-remove-empty-lines-from-a-python-string
-curationMountainDiv  = os.linesep.join([s for s in curationMountainDiv.splitlines() if s])
-
-print(curationMountainDiv)
+#scrapeo las primeras 3 notas
+for i in linksParaScrappear[0:3]:
+    page = requests.get(i)
+    soup = BeautifulSoup(page.content, "html.parser")
+    curationMountainDiv = soup.find('div', {'class', 'post-content entry-content js_entry-content'}).text
+    #brujeria que borra emptylines https://stackoverflow.com/questions/1140958/whats-a-quick-one-liner-to-remove-empty-lines-from-a-python-string
+    curationMountainDiv  = os.linesep.join([s for s in curationMountainDiv.splitlines() if s])
+    print('----------------------------------------------------------------------------------------')
+    print(curationMountainDiv)
+    print('----------------------------------------------------------------------------------------')

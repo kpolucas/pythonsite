3ff76b6a2a38098c205caeededb4b100bf432c6b
chorga <kpolucas88@gmail.com>
2019-03-15 17:58:16 -0300

    empaquetado

---
+++ 4-package/vivaperon/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vivaperon',
+      version='0.1',
+      description='Training with pip',
+      url='https://github.com/kpolucas/pythonsite/tree/master/4-package/vivaperon',
+      author='Yo',
+      author_email='kpolucas88@gmail.com',
+      license='MIT',
+      packages=['vivaperon'],
+      zip_safe=False)

1904c702d11d5dcb97dc8f6667a7eaff6319e603
root <root@chorga.chorga>
2019-04-09 10:57:05 -0300

    blackjack

--- 3-blackjack/blackjack.py
+++ 3-blackjack/blackjack.py
@@ -18,8 +18,21 @@ def handValue(cards):
             total += 1
     return total
 
-def whoWins(you,them):
-    pass
+def whoWins(you,them,firstHand):
+    if you == 21 and firstHand:
+        return 'BLACKJACK'
+    if you > 21 and them > 21:
+        return 'DRAW'
+    if you == them:
+        return 'PUSH'
+    if you > 21 and them <= 21:
+        return 'YOU BUSTED, YOU LOSE'
+    if you <= 21 and them > 21:
+        return 'THEY BUSTED, YOU WIN'
+    if you > them:
+        return 'YOU WIN'
+    else:
+        return 'YOU LOSE'
 
 deck = [
 2,3,4,5,6,7,8,9,'J','Q','K','A',
@@ -35,8 +48,9 @@ playerHand.append(deck.pop(0))
 playerHand.append(deck.pop(0))
 casinoHand.append(deck.pop(0))
 casinoHand.append(deck.pop(0))
+firstHand = True
 
-while True:
+while True: #cambiar while true por while anothercard???
     os.system('clear')
     print('Your hand   [{}] {}'.format(']['.join(str(p) for p in playerHand),handValue(playerHand)))
     print('Casino hand [{}][{}]'.format(casinoHand[0],'?'))
@@ -44,6 +58,7 @@ while True:
     anotherCard = input()
     if anotherCard == 'y':
         playerHand.append(deck.pop(0))
+        firstHand == False
     if anotherCard == 'n':
         break
 while handValue(casinoHand) <= 16:
@@ -53,5 +68,5 @@ os.system('clear')
 print('Your hand   [{}] {}'.format(']['.join(str(p) for p in playerHand),handValue(playerHand)))
 print('Casino hand [{}] {}'.format(']['.join(str(p) for p in casinoHand),handValue(casinoHand)))
 
-#Codear whoWins
-#whoWins(handValue(yourHand),handValue(casinoHand))
+print(whoWins(handValue(playerHand),handValue(casinoHand),firstHand))
+#https://en.wikipedia.org/wiki/Blackjack
